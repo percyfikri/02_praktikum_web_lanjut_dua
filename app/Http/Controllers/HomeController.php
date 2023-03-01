0ac0679e31acb12138ea98268da560d01d504aa5
percyfikri <mafikri27@gmail.com>
2023-03-01 10:36:29 +0700

    Add home, about, article, page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        echo "Selamat Datang";
+    }
+
+    public function about(){
+        echo "NIM   : 2141720190<br>";
+        echo "Nama  : Muhammad Alvi Fikri<br>";
+        echo "Kelas : 2H";
+    }
+
+    public function articles($id){
+        echo "Halaman Artikel dengan ID $id";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/index', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
